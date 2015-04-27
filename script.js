4fe2e6e930a0e96fa9b88f0e9cc1c656cfa233ad
Fully34 <cfullinw34@gmail.com>
2015-04-27 11:09:51 -0600

    generating grid, but not very well

--- script.js
+++ script.js
@@ -8,18 +8,28 @@ $(document).ready(function(){
 		}
 	};
 
+	
 	//VERIFY isInt FUNCTION
 		// console.log(isInt('3'));
 		// console.log(isInt(3));
 		// console.log(isInt('hello'));
 
+	//===============================================================//
+	// VARIABLE DECLARATIONS
+
 	var grid = prompt('What size would you like your grid?');
+	var container = $('.container');
+	// var container_width = 
+	// var container_height =
 
 	//VERIFY grid VARIABLE
 		// console.log(grid);
 		// console.log(isInt(grid));
 
-	if (grid > 30) {
+	//===============================================================//
+	//VERIFICATION THAT 'grid' IS AN INTEGER
+
+	if (grid > 100) {
 		alert('That number needs to be smaller!');
 	}else if(isInt(grid)===true){
 		alert('Grid of ' + grid + 'x' + grid + ' incoming!');
@@ -29,9 +39,25 @@ $(document).ready(function(){
 		//console.log(isInt(grid)); --> VERIFICATION OF GRID VARIABLE (WHETHER OR NOT IT'S AN INT)
 	};
 
+	//===============================================================//
+	//Iterate grid
 
-	$('.container').append('<div class="square"></div>');
+		// function gen_divs(grid){
+		for (var i = 0; i< grid; i++){
+			for (var x=0; x < grid; x++){
+			container.append($('<div></div>').addClass('square').css({"width": 20, "height": 20}))
+			.css({"width": (grid*grid), "height": (grid*grid)}); //--> need to use this to add apropriate amount of <div>'s to container <div>
+			}
+		}
 
+	//===============================================================//
+	//HOVER ACTION
 
+		$('.square').on('mouseenter', function (){
+			$(this).css('background-color', '#c9eac6');
+		});
+	// }
 
-});
+	//===============================================================//
+
+});	
