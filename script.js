6df2f3579d3da3ed6f846312adbe376d08c49a90
Fully34 <cfullinw34@gmail.com>
2015-04-27 12:51:04 -0600

    everything working pretty well, just need to clean up

--- script.js
+++ script.js
@@ -1,85 +1,83 @@
 $(document).ready(function(){
 
-	var isInt = function(num){
-		if(+num===parseInt(num)) {
-			return true
-		}else{
-			return false
-		}
-	};
+//DEFAULT TABLE
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+for (var i = 0; i< 16; i++){
+	for (var x=0; x < 16; x++){
+		$('.container').append($('<div></div>').addClass('square').css({"width": 20, "height": 20}))
+		.css({"width": 20*16, "height": 20*16}); //--> need to use this to add apropriate amount of <div>'s to container <div>
+	}
+};
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
-	
-	//VERIFY isInt FUNCTION
-		// console.log(isInt('3'));
-		// console.log(isInt(3));
-		// console.log(isInt('hello'));
+//DEFAULT TABLE ACTIONS
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+$('.square').on('mouseenter', function (){
+	$(this).css('background-color', '#f0f0f0');
+});
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
-	//===============================================================//
-	// VARIABLE DECLARATIONS
+//WHAT TO DO WHEN THE BUTTON IS PRESSED
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	$('button').on('click', function (){
+		
+		$('.container').empty(); //--> EMPTY CURRENT GRID
+		
+		//DECLARE VARIABLES --> EXCEPT FOR GRID, GRID IS LATER
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		var scale = prompt('How large would you like your squares (in pixels, up to 15)?')
 
-	//SCALE MODIFIER
-	var scale = prompt('How large would you like your squares (in pixels, up to 30)?')
 
-		//===============================================================//
-		//VERIFY THAT SCALE IS AN INTEGER < 30
-		if (scale > 30) {
-			alert('That number needs to be smaller!');
-		}else if(isInt(scale)===true){
-			alert('your squares will be: ' + scale + 'x' + scale);
-			// console.log(isInt(scale)); --> VERIFICATION OF scale VARIABLE (WHETHER OR NOT IT'S AN INT)
+		//VERIFY IF USER INPUT IS AN INTEGER
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		var isInt = function(num){
+			if(+num===parseInt(num)) {
+				return true
+			}else{
+				return false
+			}
+		};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+		//CHECK SCALE
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		if (scale > 15) {
+			alert('That number needs to be smaller!  I\'m going to be so slow!');
 		}else if(isInt(scale)!==true){
 			alert('I need an integer!')
-			//console.log(isInt(scale)); --> VERIFICATION OF scale VARIABLE (WHETHER OR NOT IT'S AN INT)
 		};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
-	var grid = prompt('What size would you like your grid?');
-	var container = $('.container');
-	var container_width = grid*scale
-	var container_height = grid *scale
-
-		//===============================================================//
+		
+		//DECLARE OTHER VARIABLES
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		var grid = prompt('What size would you like your grid?');
+		var container = $('.container');
+		var container_width = grid*scale//--> Maintains grid integrity
+		var container_height = grid *scale //--> Maintains grid integrity
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		
 		//VERIFICATION THAT 'grid' IS AN INTEGER
-
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 		if (grid > 100) {
 			alert('That number needs to be smaller!');
-		}else if(isInt(grid)===true){
+		}else if(isInt(grid)!==true){
+			alert('Stop wasting everybody\'s time!')
+		}else if(grid>100){
+			alert('Stop wasting everybody\'s time!')			
+		}else if(isInt(grid)===true&&grid<=100){
 		//===============================================================//
 		//Iterate grid
 
-			alert('Your grid will be ' + grid + 'x' + grid + '!');
-			// console.log(isInt(grid)); --> VERIFICATION OF GRID VARIABLE (WHETHER OR NOT IT'S AN INT)
-			// function gen_divs(grid){
 			for (var i = 0; i< grid; i++){
 				for (var x=0; x < grid; x++){
 				container.append($('<div></div>').addClass('square').css({"width": scale, "height": scale}))
 				.css({"width": container_width, "height": container_height}); //--> need to use this to add apropriate amount of <div>'s to container <div>
 				}
-			}
-
-		//===============================================================//
-		//HOVER ACTION
-
 			$('.square').on('mouseenter', function (){
 				$(this).css('background-color', '#f0f0f0');
 			});
-		// }
-
-		//===============================================================//
-		}else if(isInt(grid)!==true){
-			alert('I need an integer!')
-			//console.log(isInt(grid)); --> VERIFICATION OF GRID VARIABLE (WHETHER OR NOT IT'S AN INT)
+			}
 		};
-
-
-	//VERIFY grid VARIABLE
-		// console.log(grid);
-		// console.log(isInt(grid));
-
-
-
-
-
-
-
-
+	});
 });	
